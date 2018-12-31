f54eb26de60ff5d1ffe1635272947ca3a8291298
Brett Regnier <bretternestregnier@gmail.com>
2018-12-31 15:23:03 -0700

    Splash theoretically works

---
+++ Splash.ino
@@ -0,0 +1,79 @@
+class WaterDetector
+{
+	private:
+		uint8_t _pin;
+		bool _state; // true, water detected
+	public:
+	WaterDetector(uint8_t pin)
+	{
+		_pin = pin;
+	}
+	
+	void Begin()
+	{
+		_state = false;
+		pinMode(_pin, INPUT);
+	}
+	
+	bool Detect()
+	{
+		uint8_t t = digitalRead(_pin);
+		_state = (t == LOW);
+		return _state;
+	}
+};
+
+class Component
+{
+	private:
+	bool _state;
+	uint8_t _pin;
+	
+	public:
+	Component(uint8_t pin) 
+	{
+		_pin = pin;
+	}
+	
+	void Begin()
+	{
+		pinMode(_pin, OUTPUT);
+		_state = false;
+	}
+	
+	void Toggle()
+	{
+		// true, turn off | false, turn on
+		(_state) ? digitalWrite(_pin, HIGH) : digitalWrite(_pin, LOW);
+		_state = !_state;
+	}
+	
+	void Toggle(int del)
+	{
+		Toggle();
+		delay(del);
+		Toggle();
+	}
+};
+
+WaterDetector wd(13);
+Component relay(7);
+
+void setup()
+{
+	wd.Begin();
+	relay.Begin();
+	Serial.begin(1000);
+}
+
+void loop()
+{
+	// Water is not detected
+	if (!wd.Detect())
+	{
+		relay.Toggle(1000);
+	}
+	
+	// Sleep for an hour
+	delay(3600000);
+}
