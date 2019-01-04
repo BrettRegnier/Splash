325bef18a03d594ad1f745fac7a56ca4469587af
Brett Regnier <bretternestregnier@gmail.com>
2019-01-03 19:47:30 -0700

    Now in production, need to watch for over watering

--- Splash.ino
+++ Splash.ino
@@ -1,20 +1,21 @@
 class WaterDetector
 {
-	private:
-		uint8_t _pin;
-		bool _state; // true, water detected
-	public:
+  private:
+	uint8_t _pin;
+	bool _state; // true, water detected
+
+  public:
 	WaterDetector(uint8_t pin)
 	{
 		_pin = pin;
 	}
-	
+
 	void Begin()
 	{
 		_state = false;
 		pinMode(_pin, INPUT);
 	}
-	
+
 	bool Detect()
 	{
 		// water detected t = LOW, and t = HIGH water undetected
@@ -26,67 +27,78 @@ class WaterDetector
 
 class Component
 {
-	private:
+  private:
 	bool _state;
 	uint8_t _pin;
-	
-	public:
-	Component(uint8_t pin) 
+
+  public:
+	Component(uint8_t pin)
 	{
 		_pin = pin;
 	}
-	
+
 	void Begin()
 	{
 		pinMode(_pin, OUTPUT);
 		_state = false;
+		TurnOff();
 	}
-	
+
 	void Toggle()
 	{
 		// true, turn off | false, turn on
 		(_state) ? digitalWrite(_pin, HIGH) : digitalWrite(_pin, LOW);
 		_state = !_state;
 	}
-	
+
 	void Toggle(int del)
 	{
 		Toggle();
 		delay(del);
 		Toggle();
 	}
+
+	void TurnOff()
+	{
+		_state = false;
+		digitalWrite(_pin, HIGH);
+	}
 };
 
 const int DETECTORS = 2;
 
-WaterDetector resevoirDetector(8);
-WaterDetector detectors[2] = {WaterDetector(7), WaterDetector(13)};
-Component relay(7);
+WaterDetector resevoirDetector(3);
+WaterDetector detectors[DETECTORS] = {WaterDetector(12), WaterDetector(13)};
+Component relay(2);
 
 void setup()
 {
-	wd.Begin();
 	relay.Begin();
-	Serial.begin(1000);
+	for (int i = 0; i < DETECTORS; i++)
+		detectors[i].Begin();
+	resevoirDetector.Begin();
+	Serial.begin(3000);
 }
 
 void loop()
 {
-	// Water is not detected
+	// There must be water in the resevoir to proceed
 	if (resevoirDetector.Detect())
 	{
 		bool needsWatering = true;
-		for (int i = 0; i < 2; i++)
-		{
-			needsWatering = (needsWatering && !detectors[i].Detect()); 
-		}
-		
+		for (int i = 0; i < DETECTORS; i++)
+			needsWatering = (needsWatering && !detectors[i].Detect());
+
 		if (needsWatering)
 		{
-			relay.Toggle(1000);
-		}	
+			relay.Toggle(5000);
+		}
 	}
-	
+	else
+	{
+		relay.TurnOff();
+	}
+
 	// Sleep for an hour
-	delay(3600000);
+	delay(360000);
 }
