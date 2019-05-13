ba7010265498e415fead211213c78950e181d0b0
Brett Regnier <bretternestregnier@gmail.com>
2019-05-13 10:02:06 -0600

    updated to read analog data and readjust how the sleep works

--- Splash.ino
+++ Splash.ino
@@ -1,122 +1,147 @@
 class WaterDetector
 {
   private:
-	uint8_t _pin;
-	bool _state; // true, water detected
+    uint8_t _digitalPin;
+    uint8_t _analogPin;
+    bool _state; // true, water detected
 
   public:
-	WaterDetector(uint8_t pin)
-	{
-		_pin = pin;
-	}
+    WaterDetector(uint8_t digitalPin, uint8_t analogPin)
+    {
+      _digitalPin = digitalPin;
+      _analogPin = analogPin;
+    }
 
-	void Begin()
-	{
-		_state = false;
-		pinMode(_pin, INPUT);
-	}
+    void Begin()
+    {
+      _state = false;
+      pinMode(_digitalPin, INPUT);
+    }
 
-	bool Detect()
-	{
-		// water detected t = LOW, and t = HIGH water undetected
-		uint8_t t = digitalRead(_pin);
-		_state = (t == LOW);
-		return _state;
-	}
+    bool Detect()
+    {
+      // water detected t = LOW, and t = HIGH water undetected
+      uint8_t t = digitalRead(_digitalPin);
+      _state = (t == LOW);
+      return _state;
+    }
+
+    void Read(int *data, int readTime)
+    {
+      // TODO fix this garbage.
+      // not returning the pointer correctly and causing the program to crash
+      for (int i = 0; i < readTime; i++)
+      {
+        data[i] = analogRead(_analogPin);
+        Serial.println(data[i]);
+      }
+    }
 };
 
 class Component
 {
   private:
-	bool _state;
-	uint8_t _pin;
+    bool _state;
+    uint8_t _pin;
 
   public:
-	Component(uint8_t pin)
-	{
-		_pin = pin;
-	}
+    Component(uint8_t pin)
+    {
+      _pin = pin;
+    }
 
-	void Begin()
-	{
-		pinMode(_pin, OUTPUT);
-		_state = false;
-		TurnOff();
-	}
+    void Begin()
+    {
+      pinMode(_pin, OUTPUT);
+      _state = false;
+      TurnOff();
+    }
 
-	void Toggle()
-	{
-		// true, turn off | false, turn on
-		(_state) ? digitalWrite(_pin, HIGH) : digitalWrite(_pin, LOW);
-		_state = !_state;
-	}
+    void Toggle()
+    {
+      // true, turn off | false, turn on
+      (_state) ? digitalWrite(_pin, HIGH) : digitalWrite(_pin, LOW);
+      _state = !_state;
+    }
 
-	void Toggle(int del)
-	{
-		Toggle();
-		delay(del);
-		Toggle();
-	}
+    void Toggle(int del)
+    {
+      Toggle();
+      delay(del);
+      Toggle();
+    }
 
-	void TurnOff()
-	{
-		_state = false;
-		digitalWrite(_pin, HIGH);
-	}
-	
-	void TurnOn()
-	{
-		_state = true;
-		digitalWrite(_pin, LOW);
-	}
+    void TurnOff()
+    {
+      _state = false;
+      digitalWrite(_pin, HIGH);
+    }
+
+    void TurnOn()
+    {
+      _state = true;
+      digitalWrite(_pin, LOW);
+    }
 };
 
 const int DETECTORS = 2;
 
-WaterDetector resevoirDetector(5);
-WaterDetector detectors[DETECTORS] = {WaterDetector(12), WaterDetector(13)};
+WaterDetector resevoirDetector(5, A2);
+WaterDetector detectors[DETECTORS] = {WaterDetector(12, A1), WaterDetector(13, A0)};
 Component pumpRelay(2);
 Component powerRelay(3);
 
+int sleepTime = 0;
+
 void setup()
 {
-	pumpRelay.Begin();
-	powerRelay.Begin();
-	for (int i = 0; i < DETECTORS; i++)
-		detectors[i].Begin();
-	resevoirDetector.Begin();
-	Serial.begin(3000);
+  pumpRelay.Begin();
+  powerRelay.Begin();
+  for (int i = 0; i < DETECTORS; i++)
+    detectors[i].Begin();
+  resevoirDetector.Begin();
+  Serial.begin(9600);
 }
 
 void loop()
 {
-	// Turn on power to sensors
-	// and check readings
-	powerRelay.TurnOn();
-	
-	// Wait a bit before checking to allow for readings to come in
-	delay(3000);
-	
-	// There must be water in the resevoir to proceed
-	if (resevoirDetector.Detect())
-	{
-		bool needsWatering = true;
-		for (int i = 0; i < DETECTORS; i++)
-			needsWatering = (needsWatering && !detectors[i].Detect());
+  // Turn on power to sensors
+  // and check readings
+  powerRelay.TurnOn();
+  delay(900);
+  do
+  {
+    // There must be water in the resevoir to proceed
+    if (resevoirDetector.Detect())
+    {
+      bool needsWatering = true;
+      for (int i = 0; i < DETECTORS; i++)
+        needsWatering = (needsWatering && !detectors[i].Detect());
 
-		if (needsWatering)
-		{
-			pumpRelay.Toggle(5000);
-		}
-	}
-	else
-	{
-		pumpRelay.TurnOff();
-	}
-	
-	// turn off the power relay 
-	powerRelay.TurnOff();
+      if (needsWatering)
+      {
+        pumpRelay.Toggle();
+        sleepTime = 5000;
+      }
+    }
+    else
+    {
+      pumpRelay.TurnOff();
+      sleepTime = 0 ;
+    }
+  } while (sleepTime-- > 0);
 
-	// Sleep for an hour
-	delay(3600000);
+  delay(900);
+  int readTime = 10;
+  int data[readTime];
+  detectors[0].Read(&data[0], readTime);
+  //  for (int i = 0; i < readTime; i++)
+  //    Serial.println(data[i]);
+
+
+  // turn off the power relay
+  powerRelay.TurnOff();
+
+  // Sleep for an hour
+  delay(3600000);
 }
